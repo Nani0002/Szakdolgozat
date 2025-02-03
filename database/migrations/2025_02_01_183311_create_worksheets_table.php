8f90a7e450ea574b417ec76b435f6465b90a58fe
Nani0002 <molnardani002@gmail.com>
2025-02-03 19:19:20 +0100

    Add basic db migrations

---
+++ database/migrations/2025_02_01_183311_create_worksheets_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('worksheets', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger("customer_id")->nullable();
+            $table->foreign("customer_id")->references("id")->on("customers")->onDelete("cascade");
+
+            $table->string("sheet_number");
+            $table->enum("sheet_type", ["maintanance", "paid", "warranty"]);
+            $table->date("print_date")->nullable();
+            $table->date("declaration_time");
+            $table->enum("declaration_mode", ["email", "phone", "personal", "onsite"]);
+            $table->string("error_description");
+            $table->string("comment");
+            $table->string("condition");
+
+            $table->unsignedBigInteger("coworker_id")->nullable();
+            $table->foreign("coworker_id")->references("id")->on("users")->onDelete("cascade");
+            $table->unsignedBigInteger("liable_id")->nullable();
+            $table->foreign("liable_id")->references("id")->on("users")->onDelete("cascade");
+
+            $table->date("work_start");
+            $table->date("work_end")->nullable();
+            $table->integer("worktme");
+            $table->text("work_description");
+
+            $table->unsignedBigInteger("outsourcing_id")->nullable();
+            $table->foreign("outsourcing_id")->references("id")->on("outsourcings")->onDelete("cascade");
+
+            $table->enum("current_step", ["started", "distributed", "ongoing", "price_offered", "waiting", "to_invoice", "closed"]);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('worksheets');
+    }
+};
---
+++ database/migrations/2025_02_03_175819_create_outsourcings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('outsourcings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger("company_id")->nullable();
+            $table->foreign("company_id")->references("id")->on("companies")->onDelete("cascade");
+
+            $table->date("entry_time");
+            $table->string("outsourced_number");
+            $table->double("outsourced_price");
+            $table->double("our_price");
+            $table->enum("finished", ["ongoing", "finished", "brought"]);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('outsourcings');
+    }
+};
