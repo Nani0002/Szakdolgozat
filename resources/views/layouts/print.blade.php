829d002728cfd5bb6a8b8b2575266f0fcf2a9e2a
Nani0002 <molnardani002@gmail.com>
2025-04-09 09:28:14 +0200

    Wrappers and outsourcing prints

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -8,6 +8,39 @@
     <title>Print - {{ $worksheet->sheet_number }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        @media print {
+
+            html,
+            body {
+                height: auto;
+            }
+
+            .print-signature-wrapper {
+                page-break-inside: avoid;
+                margin-top: auto;
+                padding-top: 2rem;
+                display: block;
+            }
+
+            #signature {
+                page-break-inside: avoid;
+                page-break-after: auto;
+            }
+
+            .container-fluid {
+                display: flex;
+                flex-direction: column;
+                min-height: 100vh;
+            }
+
+            .wrapper {
+                page-break-inside: avoid;
+                page-break-inside: avoid;
+                page-break-after: auto;
+            }
+        }
+    </style>
 </head>
 
 <body class="p-2">
@@ -148,26 +181,28 @@
                     <div class="col-3">{{ $type }}</div>
                 </div>
                 @if (isset($worksheet->computers) && count($worksheet->computers) > 0)
-                    <div class="row">
-                        <div class="col-12 fw-bold text-decoration-underline">Beadott Számítógép@php echo count($worksheet->computers) > 1 ? 'ek:' : ':' @endphp</div>
+                    <div class="wrapper">
+                        <div class="row">
+                            <div class="col-12 fw-bold text-decoration-underline">Beadott Számítógép@php echo count($worksheet->computers) > 1 ? 'ek:' : ':' @endphp
+                            </div>
+                        </div>
+                        @foreach ($worksheet->computers as $computer)
+                            <div class="row">
+                                <div class="col-3 fw-bold">Sorozatzám:</div>
+                                <div class="col-9">{{ $computer->serial_number }}</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-3 fw-bold">Gyártó:</div>
+                                <div class="col-3">{{ $computer->manufacturer }}</div>
+                                <div class="col-3 fw-bold">Típus:</div>
+                                <div class="col-3">{{ $computer->type }}</div>
+                            </div>
+                            <div class="row">
+                                <div class="col-3 fw-bold">Állapot:</div>
+                                <div class="col-9">{{ $computer->pivot->condition }}</div>
+                            </div>
+                        @endforeach
                     </div>
-                    @foreach ($worksheet->computers as $computer)
-                        <div class="row">
-                            <div class="col-3 fw-bold">Sorozatzám:</div>
-                            <div class="col-9">{{ $computer->serial_number }}</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-3 fw-bold">Gyártó:</div>
-                            <div class="col-3">{{ $computer->manufacturer }}</div>
-                            <div class="col-3 fw-bold">Típus:</div>
-                            <div class="col-3">{{ $computer->type }}</div>
-                        </div>
-                        <div class="row">
-                            <div class="col-3 fw-bold">Állapot:</div>
-                            <div class="col-9">{{ $computer->pivot->condition }}</div>
-
-                        </div>
-                    @endforeach
                 @endif
                 <div class="row mt-2">
                     <div class="col-6">Hiba / feladat leírása:</div>
@@ -179,43 +214,88 @@
                 </div>
             </div>
         </div>
-        <div class="row border border-dark pt-2">
+        <div class="row border border-dark pt-2 wrapper">
             <div class="col-6">
                 <div class="row">
                     <div class="col-12 fw-bold text-decoration-underline">Belső munkaleírás:</div>
                 </div>
                 <div class="row mt-2">
                     <div class="col-6 fw-bold">Belső munkatárs:</div>
-                    <div class="col-6">{{$worksheet->coworker->name}}</div>
+                    <div class="col-6">{{ $worksheet->coworker->name }}</div>
                 </div>
                 <div class="row">
                     <div class="col-6 fw-bold">Belső felelős:</div>
-                    <div class="col-6">{{$worksheet->liable->name}}</div>
+                    <div class="col-6">{{ $worksheet->liable->name }}</div>
                 </div>
                 <div class="row">
                     <div class="col-6 fw-bold">Munka kezdete:</div>
-                    <div class="col-6">{{$worksheet->work_start}}</div>
+                    <div class="col-6">{{ $worksheet->work_start }}</div>
                 </div>
                 <div class="row">
                     <div class="col-6 fw-bold">Munka vége:</div>
-                    <div class="col-6">{{$worksheet->work_end}}</div>
+                    <div class="col-6">{{ $worksheet->work_end }}</div>
                 </div>
                 <div class="row">
                     <div class="col-6 fw-bold">Munkaidő:</div>
-                    <div class="col-6">{{$worksheet->work_time}}</div>
+                    <div class="col-6">{{ $worksheet->work_time }}</div>
                 </div>
             </div>
-            <div class="col-6">
+            <div class="col-6 d-flex align-items-stretch">
                 <div class="row">
                     <div class="col-5 fw-bold">Belső munkaleírás:</div>
-                    <div class="col-7 border border-dark">{{$worksheet->work_description}}</div>
+                    <div class="col-7 border border-dark">{{ $worksheet->work_description }}</div>
                 </div>
             </div>
         </div>
+        @if (isset($worksheet->outsourcing))
+            <div class="wrapper">
+                <div class="row border border-dark border-bottom-0 bg-secondary-subtle fw-bold">
+                    <div class="col-4">Külső munkalap</div>
+                    <div class="col-3">outsourced_price</div>
+                    <div class="col-3">our_price</div>
+                    <div class="col-2">finished</div>
+                </div>
+                <div class="row border-bottom border-dark">
+                    <div class="col-4">
+                        {{ $worksheet->outsourcing->outsourced_number }}<br>{{ $worksheet->outsourcing->company->name }}
+                    </div>
+                    <div class="col-3">{{ $worksheet->outsourcing->outsourced_price }} Ft.</div>
+                    <div class="col-3">{{ $worksheet->outsourcing->our_price }} Ft.</div>
+                    <div class="col-2">
+                        @switch($worksheet->outsourcing->finished)
+                            @case('ongoing')
+                                Munka alatt
+                            @break
 
-        <div class="row mt-5 mx-2">
-            <div class="col-3 border-top border-2 border-dark text-center">Átadó ({{$worksheet->customer->name}})</div>
-            <div class="col-3 offset-6 border-top border-2 border-dark text-center">Kiállító ({{$worksheet->liable->name}})</div>
+                            @case('finished')
+                                Elkészült
+                            @break
+
+                            @case('brought')
+                                Elhozva
+                            @break
+                        @endswitch
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="print-signature-wrapper">
+            <div class="row mt-5 mx-2" id="signature">
+                <div class="col-3 border-top border-2 border-dark text-center">
+                    Átadó
+                    @if (strlen($worksheet->customer->name) > 10)
+                        <br>
+                    @endif
+                    ({{ $worksheet->customer->name }})
+                </div>
+                <div class="col-3 offset-6 border-top border-2 border-dark text-center">
+                    Kiállító
+                    @if (strlen($worksheet->liable->name) > 10)
+                        <br>
+                    @endif
+                    ({{ $worksheet->liable->name }})
+                </div>
+            </div>
         </div>
     </div>
 
